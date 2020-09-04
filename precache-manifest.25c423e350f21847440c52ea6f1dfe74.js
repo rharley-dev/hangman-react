44dad2ab9219b48dd405ef0addf02041f4385611
Ryan Harley <rharley.dev@gmail.com>
2020-09-04 12:27:52 -0500

    Updates

---
+++ precache-manifest.25c423e350f21847440c52ea6f1dfe74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d3c308225873435445c5cc9aca621d9",
+    "url": "/hangman-react/index.html"
+  },
+  {
+    "revision": "695e049e69b098e76811",
+    "url": "/hangman-react/static/css/main.896e1964.chunk.css"
+  },
+  {
+    "revision": "f3293ff065b04177a034",
+    "url": "/hangman-react/static/js/2.0e429a6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman-react/static/js/2.0e429a6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "695e049e69b098e76811",
+    "url": "/hangman-react/static/js/main.23689e89.chunk.js"
+  },
+  {
+    "revision": "3f7c69ac9d5e568f51ba",
+    "url": "/hangman-react/static/js/runtime-main.0e2bf36f.js"
+  }
+]);
